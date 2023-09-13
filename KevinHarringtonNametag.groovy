f09c1143d405e9563886d45879e6d8c21a7c6a43
Kevin Harrington <mad.hephaestus@gmail.com>
2023-09-13 11:16:17 -0400

    adding name

--- KevinHarringtonNametag.groovy
+++ KevinHarringtonNametag.groovy
@@ -1,4 +1,14 @@
-// code here
+import eu.mihosoft.vrl.v3d.*;
+import javafx.scene.text.Font;
+
+Font font = new Font("Arial",  30);
+
+CSG text = CSG.unionAll(
+	TextExtrude.text((double)2,"Kevin!",font).collect{
+	    it.rotx(180)
+	    .toZMin()
+     }
+)
 
 double width = 30
 double depth= 70
@@ -10,4 +20,4 @@ println "depth is "+depth
 
 CSG nameTagBase = new Cube(width,depth,height).toCSG()
 
-return nameTagBase
+return [nameTagBase,text]
