8b09e9c034be3dbc9a78bdf11a1dbc95eb84587d
Kevin harrington <mad.hephaestus@gmail.com>
2023-09-18 14:42:04 -0400

    Most of my nametag
    got demonstration

--- KevinHarringtonNametag.groovy
+++ KevinHarringtonNametag.groovy
@@ -1,23 +1,32 @@
-import eu.mihosoft.vrl.v3d.*;
-import javafx.scene.text.Font;
 
-Font font = new Font("Arial",  30);
+CSG text = CSG.text("Kevin!",5)
 
-CSG text = CSG.unionAll(
-	TextExtrude.text((double)2,"Kevin!",font).collect{
-	    it.rotx(180)
-	    .toZMin()
-     }
-)
+CSG rotatedText = text.rotz(90)
+
+double distanceyCenter = rotatedText.getCenterY()
+
+println "Center of Word "+distanceyCenter
+
+CSG centeredText  = rotatedText.movey(-distanceyCenter)
 
 double width = 30
 double depth= 70
 double height =3
+double widthOfText = centeredText.getTotalY()+4.0
 
+double scale = depth/widthOfText
+
+CSG scaledText = centeredText.scaley(scale)
+
+double distanceInX = scaledText.getCenterX()
+CSG textCenteredInX = scaledText.movex(-distanceInX)
+
+println "Scale for the text "+scale
+println "Total text width is "+widthOfText
 println "Width is "+width
 println "Height is "+height
 println "depth is "+depth
 
 CSG nameTagBase = new Cube(width,depth,height).toCSG()
 
-return [nameTagBase,text]
+return [nameTagBase,textCenteredInX]
